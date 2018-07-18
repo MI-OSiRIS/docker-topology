54a6ab4096faf866bf417ce70489c68b0e4246a8
root <root@vlan-2.bdf-sm093.net.uits.iuk.edu>
2018-07-18 18:51:40 +0000

    fix supervisor issue

--- run.sh
+++ run.sh
@@ -5,12 +5,9 @@ HOSTNAME=`hostname`
 git -C unisrt pull
 git -C topology pull
 
-sudo mkdir /data
-sudo mkdir /data/db
-sudo mkdir /data/db/log
-
+sudo supervisord
 sudo -E /etc/init.d/supervisor start
-sudo service supervisor restart
+
 sudo lldpd -i eth0
 sudo snmpd 
 echo "SDN Controller IP : `hostname --ip-address`"
